1b39cdd1810224e42105d61605e1d721197000a2
Mithilesh Sharma <68504882+Mithilesh-create@users.noreply.github.com>
2024-01-21 14:34:40 +0000

    adding first java file to push

---
+++ test.java
@@ -0,0 +1,12 @@
+class QuickStart {
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int x = add(10, 20);
+        System.out.println(x);
+    }
+
+}
